7d16f49db5d1f3e0656e5c71d84698b588f74cc8
Griffin <gquarles@protonmail.com>
2022-04-21 15:36:16 -0400

    Clean up visualizations

--- visualization/graph.py
+++ visualization/graph.py
@@ -2,74 +2,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def readnums(file):
+def readnums(file: str):
     f = open(f'data/{file}.txt', 'r')
-
     lines = f.readlines()
+    f.close()
+
     nums = []
     for line in lines:
         n = line.strip('\n')
         n = n.split(' ')[1]
         nums.append(int(n))
-        
-    f.close()
-
+    
     return nums
 
 class SumSet:
     def __init__(self, file):
-        self.data = readnums(file)
+        self.data = readnums(str(file))
 
-        integral = []
-        integralArea = 0
-        log = []
-        for num in self.data:
-            integralArea += num
-            integral.append(integralArea)
-            if num != 0:
-                log.append(math.log(num))
-
-        integralLog = []
-        for num in integral:
-            if num != 0:
-                integralLog.append(math.log(num))
-        
-        x = []
-        for i in range(0, len(self.data)):
-            x.append(i + 1)
-
-        self.log = log
-        self.x = x
-        self.integralLog = integralLog
-        self.integralLogTotal = math.log(integralArea)
-        self.integral = integral
-        self.integralArea = integralArea
+        #Helper array for plotting
+        self.x = [i for i in range(0, len(self.data))]
         self.derivative = np.gradient(self.data)
-    
+        self.integral = []
+        self.integralLog = []
+        self.log = []
+        self.totalSums = 0
+        
+        for num in self.data:
+            self.totalSums += num
+            self.integral.append(self.totalSums)
+            
+            if num != 0:
+                self.log.append(math.log(num))
+                self.integralLog.append(math.log(self.totalSums))
 
+#Import all sumsets 1->18
 sets = []
 for i in range(1, 18):
     sets.append(SumSet(f'{i}'))
 
-for i, numset in enumerate(sets):
-    i = i + 1
+plt.xlabel('Summation')
+plt.ylabel('Compositions')
+#Examples of plotting all imported data
+plt.title('Derivatives of lengths 1-17')
+for numset in sets:
+    plt.plot(numset.derivative)
     #plt.plot(numset.integral)
+    #plt.plot(numset.log)
+    #plt.plot(numset.integralLog)
+    #plt.plot(numset.data)
 
-d = []
-for i, num in enumerate(sets[13].integral):
-    d.append(sets[14].integral[i] + sets[13].integral[i])
-
-
-
+#Example of plotting only one dataset
 #plt.plot(sets[15].integral)
-#plt.plot(d)
-
-
-plt.plot(np.gradient(d))
-#plt.plot(sets[16].data)
-plt.plot(sets[15].data)
-plt.plot(sets[14].data)
-
-#plt.plot(sets[16].derivative)
+#plt.title('Integral for length = 16')
 
 plt.show()
