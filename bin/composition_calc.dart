fb58375394af6266dc87dd38eb15887f6fd0ffcf
Griffin Quarles <griffinq178@gmail.com>
2022-11-02 16:19:01 -0400

    refactor

--- bin/composition_calc.dart
+++ bin/composition_calc.dart
@@ -1,29 +1,45 @@
+import 'dart:io';
+
 import 'package:trotter/trotter.dart';
-
-void main(List<String> arguments) {
-  const length = 14;
-  const List<int> numberSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+void findSums(int length, List<int> numberSet) {
   Map<int, int> possibleSums = {};
+  Map<int, List<List<int>>> possibleSumsWithComps = {};
+
   for (int i = (numberSet.first * length); i <= (numberSet.last * length); i++) {
     possibleSums[i] = 0;
+    possibleSumsWithComps[i] = [];
   }
 
   var comps = Compositions(length, numberSet.toList());
-  print('${comps.length} compositions with ${possibleSums.length} possible unique summations.');
-
-  print('Finding summations...');
+  
+  print('Finding summations for n = $length...');
   for (final composition in comps()) {
     var sum = 0;
     for (final number in composition) {
       sum += number;
     }
     possibleSums[sum] = possibleSums[sum]! + 1;
+    possibleSumsWithComps[sum]!.add(composition);
   }
 
-  for (MapEntry e in possibleSums.entries) {
-    print('${e.key}: ${e.value}');
+  Map<int, List<List<int>>> sumsWithComps = {};
+  for (MapEntry e in possibleSumsWithComps.entries) {
+    sumsWithComps[e.key] = e.value;
   }
 
-  return;
+  var file = File('n$length.txt');
+  if (file.existsSync()) file.deleteSync();
+  file.createSync();
+
+  file.writeAsStringSync(sumsWithComps.toString());
+  print('saved to ${file.path}');
+}
+
+void main(List<String> arguments) {
+  const List<int> u = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  print('universe = $u');
+
+  for (int n = 0; n <= 5; n++) {
+    findSums(n, u);
+  }
 }
